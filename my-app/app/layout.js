26e9559a9277cceaec47766678444b527d80d4a1
Shahidul Islam <shahidul1920shakil@gmail.com>
2026-08-15 20:16:36 +0600

    vercel analytics

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Space_Grotesk, Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
         <footer>
           <Footer />
         </footer>
+        <Analytics />
       </body>
     </html>
   );
